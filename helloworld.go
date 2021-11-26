8fee29c94cd724fdfcf1f8bd7c6309fe8b737442
Justmaker Hsu <justmaker@gmail.com>
2021-11-26 15:10:52 +0800

    add switch and stdin string example note: input string need TrimSpace to get precise string content

--- helloworld.go
+++ helloworld.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"helloworld/hello"
+	"os"
+	"strings"
 )
 
 func main() {
+	fmt.Print("Ouput mode? [i]mport/[c]onst/[v]ariable: ")
+	reader := bufio.NewReader(os.Stdin)
+	delim := byte('\n')
+	text, err := reader.ReadString(delim)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	} else {
+		fmt.Print("text is: ", text)
+	}
+
 	hello_vstr := "Hello String Variable"
-	const hello_str = "Hello"
-	hello.Hello()
-	fmt.Println(hello_vstr, hello_str)
+	const hello_cstr = "Hello"
+
+	switch text = strings.TrimSpace(text); text {
+	case "i", "I", "import", "Import":
+		hello.Hello()
+	case "c", "C", "const", "Const":
+		fmt.Println(hello_cstr)
+	case "v", "V", "variable", "Variable":
+		fmt.Println(hello_vstr)
+		fallthrough
+	default:
+		fmt.Println("Program exit.")
+	}
 }
