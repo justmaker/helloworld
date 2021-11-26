e7784de01cb62b10dcc28eec444528125b2641e2
Justmaker Hsu <justmaker@gmail.com>
2021-11-26 11:56:16 +0800

    add variable and constant string example

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,13 @@
 package main
 
-import "helloworld/hello"
+import (
+	"fmt"
+	"helloworld/hello"
+)
 
 func main() {
+	hello_vstr := "Hello String Variable"
+	const hello_str = "Hello"
 	hello.Hello()
+	fmt.Println(hello_vstr, hello_str)
 }
